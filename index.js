6c210a3c1badc851f28c94014ab8149d35428c17
mns0105 <manish0108.kr@gmail.com>
2022-08-06 14:28:02 +0530

    counter app

---
+++ index.js
@@ -0,0 +1,55 @@
+//document.getElementById("count-el").innerText=5;
+// let dreamerBatch=50;
+// let OnionBatch=30;
+// let count=dreamerBatch+OnionBatch;
+// console.log(count);
+// let bonuspoint=50;
+// console.log(bonuspoint);
+//  bonuspoint =bonuspoint +50;
+//  console.log(bonuspoint);
+// bonuspoint= bonuspoint-57;
+// console.log(bonuspoint);
+// bonuspoint=bonuspoint+45;
+//    voting app
+/*function voter1(){
+    
+
+    console.log("BJP");
+    }
+
+function voter2(){
+console.log("SAPA");
+} */   
+// function countdown(){
+    // console.log(1);
+    // console.log(2);
+    // console.log(3);
+    // console.log(4);
+
+// }
+// countdown();
+// countdown();
+/*function mylogger()
+{
+    console.log(42);
+}
+mylogger()*/
+// let Lap=0;
+
+// function totalLap(){
+
+// let Lap=Lap+1;
+// console.log(totalLap);
+// }
+let countEl=document.getElementById("count-el") //
+console.log(countEl)
+let count=0;
+function save(){
+    console.log(count)
+}
+save();
+function increment(){
+    count=count+1;
+    console.log(count);
+countEl.innerText=count;
+}
